c961f0e850913d3d6a1fbec44b155ac2a2d72937
Birensrk <iambirensrk@gmail.com>
2025-05-28 21:03:38 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as newer js version
+
+// alert(3 + 3) // we are using nodejs, not browser 
+
+console.log(3 
+    + 
+    3) // code readability should be high
+
+console.log("Hitesh")
+
+let name = "Hitesh"
+let age = 18
+let IsLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined =>
+// symbol => unique
+
+// object 
+
+console.log(typeof age);
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object 
+
