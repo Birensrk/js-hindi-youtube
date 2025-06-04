ee47a471275e390ec8ee5ede706d60fd0c57bf1b
Birensrk <iambirensrk@gmail.com>
2025-06-04 00:25:34 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "Hitesh"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33 (Number can easily convert into Number 33)
+// "33abc" => NaN NaN(Not in Nymber but NaN ka type joh hain Number hi hain.)
+// true => 1; false => 0
+
+let IsLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+// console.log( booleanIsLoggedIn);
+
+// 1 => true; 0 => false;
+// "" => false
+//"hitesh" => true
+
+let SomeNumber = 33
+
+let stringNumber = String(SomeNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
