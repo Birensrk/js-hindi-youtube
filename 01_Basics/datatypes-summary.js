5d87dc9d3cb5fa9e15a0748a4303da8881934a7f
Birensrk <iambirensrk@gmail.com>
2025-06-10 13:45:11 +0000

    datatypes summary

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,40 @@
+// primitive
+
+// 7 types : String, Number, Boolean, null, undefined, symbol, BigInt,
+
+const score = 100
+const scoreValue = 100.3
+
+const IsLoggedIn = false
+const outsiudetemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 35474857366555677887766666666767n
+
+
+// reference (Non-Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "nagraaj", "doga"]
+
+let myObj = {
+    name: "hitesh",
+    age: 21,
+}
+
+const myFunction = function(){
+    console.log("hello world");
+    
+}
+
+console.log(typeof scoreValue);
+
+
+
+
